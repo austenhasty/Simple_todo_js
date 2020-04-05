32ca2a9cd01ad987ba7c6998c1d63142c0db4309
austenhasty <hastyae@gmail.com>
2020-04-05 10:06:01 -0700

    added javascript

---
+++ src/index.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const form = document.getElementById('create-task-form')
+  getDate()
+  form.addEventListener('submit', addTask)
+
+
+});
+
+function getDate() {
+  const title = document.getElementById('title')
+
+  let h5 = document.createElement('h5')
+  let date = new Date().toLocaleDateString()
+  console.log(date)
+  h5.textContent = date
+  title.appendChild(h5)
+}
+
+function addTask(ev) {
+  ev.preventDefault()
+  const newTaskDescription = document.getElementById('new-task-description')
+  const newTask = document.createElement('li')
+  newTask.textContent = newTaskDescription.value + ' '
+  appendTask(newTask)
+}
+
+function appendTask(newTask) {
+  const list = document.getElementById('list')
+  const deleteBtn = document.createElement('button')
+  const newTaskDescription = document.getElementById('new-task-description')
+
+  deleteBtn.textContent = 'Delete'
+
+  list.appendChild(newTask)
+  newTask.appendChild(deleteBtn)
+  newTaskDescription.value = ''
+
+  deleteBtn.addEventListener('click', () => {
+    list.removeChild(newTask)
+  })
+}
+
+// function deleteTask() {
+//
+//   newTask.parentremoveChild(list.lastchild)
+// }
